186f86cdb162ff4616082b5bc019db52e6b4a4c9
Jesus <jesuseduardoperezvargas@gmail.com>
2022-05-27 13:54:43 -0300

    Comenzando de nuevo el proyecto

--- proyectobase/proyectobase/urls.py
+++ proyectobase/proyectobase/urls.py
@@ -1,4 +1,4 @@
-"""Familia URL Configuration
+"""proyectobase URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.0/topics/http/urls/
--- proyectobase/proyectobase/wsgi.py
+++ proyectobase/proyectobase/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for Familia project.
+WSGI config for proyectobase project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Familia.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'proyectobase.settings')
 
 application = get_wsgi_application()
