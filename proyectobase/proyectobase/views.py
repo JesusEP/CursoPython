470b2b9a9c797f92227dff4ac8f9a5cd98ee8233
Jesus <jesuseduardoperezvargas@gmail.com>
2022-05-27 18:02:41 -0300

    Mejorando template

--- proyectobase/proyectobase/views.py
+++ proyectobase/proyectobase/views.py
@@ -1,15 +1,22 @@
 from fileinput import close
 from pipes import Template
+from re import template
 from django.http import HttpResponse
 from django.template import Template, Context
+from django.shortcuts import render
 
 def familia (request):
     return HttpResponse('Probando el familia')
 
-def probandoTemplate(self):
-    miHtml = open("C:/Users/jperez/Documents/Django/proyectobase/templates/template1.html")
-    plantilla = Template(miHtml.read())
-    miHtml.close()
-    miContexto = Context()
-    documento = plantilla.render(miContexto)
-    return HttpResponse(documento)
+def probandoTemplate(request):
+    context = {
+        'nombre':'Jesus',
+        'apellido':'Perez'
+    }
+    return render(request, 'template1.html', context = context)
+    # #miHtml = open("C:/Users/jperez/Documents/Django/proyectobase/templates/template1.html")
+    # plantilla = Template(miHtml.read())
+    # miHtml.close()
+    # miContexto = Context()
+    # # documento = plantilla.render(miContexto)
+    # # return HttpResponse(documento)
